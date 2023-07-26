5661ec366caab2b70fa04d317b0758e75e4904a7
Denis Karpelevich <dkarpele@redhat.com>
2023-07-26 20:38:12 +0200

    Prepare base structure 2
    Signed-off-by: Denis Karpelevich <dkarpele@redhat.com>

--- users_analyze_api/src/core/config.py
+++ users_analyze_api/src/core/config.py
@@ -16,68 +16,7 @@ class MainConf(BaseSettings):
 
 class Settings(MainConf):
     project_name: str = Field(..., env='PROJECT_NAME')
-    redis_host: str = Field(..., env='REDIS_HOST')
-    redis_port: int = Field(..., env='REDIS_PORT')
-    host: str = Field(..., env='HOST_AUTH')
-    port: int = Field(..., env='PORT_AUTH')
-    secret_key: str = Field(..., env='SECRET_KEY')
-    secret_key_refresh: str = Field(..., env='SECRET_KEY_REFRESH')
 
 
 settings = Settings()
 
-
-class DBCreds(MainConf):
-    dbname: str = Field(..., env="DB_NAME")
-    user: str = Field(..., env="DB_USER")
-    password: str = Field(..., env="DB_PASSWORD")
-    host: str = Field(env="DB_HOST", default='127.0.0.1')
-    port: int = Field(env="DB_PORT", default=5432)
-    options: str = '-c search_path=%s' % os.environ.get('PG_SCHEMA')
-
-
-database_dsn = DBCreds()
-
-
-class YandexCreds(MainConf):
-    client_id: str = Field(..., env='YA_CLIENT_ID')
-    secret: str = Field(..., env='YA_SECRET')
-
-
-yandex_config = YandexCreds()
-
-
-class GoogleCreds(MainConf):
-    client_id: str = Field(..., env='GOOGLE_CLIENT_ID')
-    secret: str = Field(..., env='GOOGLE_SECRET')
-
-
-google_config = GoogleCreds()
-
-
-class JaegerCreds(MainConf):
-    start: str = Field(..., env='JAEGER_START')
-    agent_host_name: str = Field(..., env='JAEGER_AGENT_HOST_NAME')
-    agent_port: str = Field(..., env='JAEGER_AGENT_PORT')
-
-
-jaeger_config = JaegerCreds()
-
-
-class RateLimit(MainConf):
-    request_limit_per_minute: int = Field(env="REQUEST_LIMIT_PER_MINUTE",
-                                          default=20)
-    is_rate_limit: bool = (os.getenv('IS_RATE_LIMIT', 'False') == 'True')
-
-
-rl = RateLimit()
-
-LOGIN_DESC = "user's login"
-FIRST_NAME_DESC = "user's first name"
-LAST_NAME_DESC = "user's last name"
-PASSWORD_DESC = "Password: Minimum eight characters, " \
-                "at least one letter and one number:"
-ROLE_TITLE_DESC = "Roles title"
-PERMISSIONS_DESC = "Permission for the role"
-PAGE_DESC = "Номер страницы"
-SIZE_DESC = "Количество элементов на странице"
--- users_analyze_api/src/main.py
+++ users_analyze_api/src/main.py
@@ -8,31 +8,16 @@ from asgi_correlation_id.middleware import is_valid_uuid4
 from fastapi import FastAPI, Depends, Request, status
 from fastapi.responses import ORJSONResponse
 
-from api.v1 import auth, oauth, users, roles
-from core.config import settings, database_dsn, jaeger_config
+from core.config import settings
 from core.logger import LOGGING
-from db import redis, postgres
-from services.database import rate_limit
-from services.token import check_access_token
-from services.users import check_admin_user
-from tracer import configure_tracer, configure_instrument
 
 
 async def startup():
-    redis.redis = redis.Redis(host=settings.redis_host,
-                              port=settings.redis_port,
-                              ssl=False)
-    postgres.postgres = postgres.Postgres(
-                      f'postgresql+asyncpg://'
-                      f'{database_dsn.user}:{database_dsn.password}@'
-                      f'{database_dsn.host}:{database_dsn.port}/'
-                      f'{database_dsn.dbname}')
-    postgres.get_session()
+    pass
 
 
 async def shutdown():
-    await redis.redis.close()
-    await postgres.postgres.close()
+    pass
 
 
 @asynccontextmanager
@@ -42,49 +27,16 @@ async def lifespan(app: FastAPI):
     await shutdown()
 
 
-if jaeger_config.start == "True":
-    configure_tracer()
-
 app = FastAPI(
     title=settings.project_name,
-    description="Api for users auth",
+    description="Api to analyze users behaviour",
     version="1.0.0",
-    docs_url='/api/openapi-auth',
-    openapi_url='/api/openapi-auth.json',
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
-    lifespan=lifespan,
-    dependencies=[Depends(rate_limit)])
+    lifespan=lifespan)
 
-app.add_middleware(
-    CorrelationIdMiddleware,
-    header_name='X-Request-ID',
-    update_request_header=True,
-    generator=lambda: uuid.uuid4().hex,
-    validator=is_valid_uuid4,
-    transformer=lambda a: a,
-)
-
-if jaeger_config.start == "True":
-    configure_instrument(app)
-
-
-@app.middleware('http')
-async def before_request(request: Request, call_next):
-    response = await call_next(request)
-    request_id = request.headers.get('X-Request-Id')
-    if not request_id:
-        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                              content={'detail': 'X-Request-Id is required'})
-    return response
-
-
-app.include_router(auth.router, prefix='/api/v1/auth', tags=['auth'])
-app.include_router(oauth.router, prefix='/api/v1/oauth', tags=['oauth'])
-app.include_router(roles.router, prefix='/api/v1/roles', tags=['roles'],
-                   dependencies=[Depends(check_access_token),
-                                 Depends(check_admin_user)])
-app.include_router(users.router, prefix='/api/v1/users', tags=['users'],
-                   dependencies=[Depends(check_access_token)])
+# app.include_router(users.router, prefix='/api/v1/users', tags=['users'])
 
 if __name__ == '__main__':
     uvicorn.run(
